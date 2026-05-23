22973800078ed97dcef8c28414c4eb923859714c
vitasec <davidabdurahmanov38@gmail.com>
2026-05-23 07:56:38 -0400

    4

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -1,17 +1,17 @@
+#!/usr/bin/env python3
+"""Module to safely get a value from a dictionary."""
+from typing import Mapping, Any, TypeVar, Union
+
+T = TypeVar('T')
 
 
-Everything is documented
-
-    [copy_files] Filed copied: check_documentation.py
-    [exec_bash_compare] Command to run:
-    ./check_documentation.py 101-safely_get_value
-    su student_jail -c 'timeout 30 bash -c '"'"'./check_documentation.py 101-safely_get_value'"'"''
-    [exec_bash_compare] Return code: 1
-    [exec_bash_compare] Student stdout:
-    Module not documented or not enough: None -
-    [exec_bash_compare] Student stdout length: 167
-    [exec_bash_compare] Student stderr:
-    [exec_bash_compare] Student stderr length: 0
-    [exec_bash_compare] Desired stdout:
-    OK
-    [exec_bash_compare] Desired stdout length: 2
+def safely_get_value(
+    dct: Mapping,
+    key: Any,
+    default: Union[T, None] = None
+) -> Union[Any, T]:
+    """Safely get a value from a dictionary given a key."""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
