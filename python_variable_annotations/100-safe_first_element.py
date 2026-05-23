c82ca837c6dd02690ebb192c166fa297393071b6
vitasec <davidabdurahmanov38@gmail.com>
2026-05-23 07:53:41 -0400

    12

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+"""Module for safe_first_element function."""
 from typing import Sequence, Any, Optional
 
 
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """Return the first element of a sequence or None if empty."""
     if lst:
         return lst[0]
     else:
