72ae24782134a9962f1997cff6c96a3f6c809476
vitasec <davidabdurahmanov38@gmail.com>
2026-05-23 07:58:04 -0400

    12

---
+++ python_variable_annotations/102-type_checking.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module that zooms an array by a given factor."""
+from typing import List, Tuple
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Zooms in an array by repeating its items."""
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
