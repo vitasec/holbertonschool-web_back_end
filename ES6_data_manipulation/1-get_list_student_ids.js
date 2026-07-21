46a1523f49a0745bb0c59a83c0555c6d2fbb5540
vitasec <davidabdurahmanov38@gmail.com>
2026-07-21 06:09:04 +0400

    feat: get student IDs using map

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,10 @@
+// List students
+
+const getListStudentIds = (ids) => {
+  if (!Array.isArray(ids)) {
+    return [];
+  }
+  const meids = ids.map((item) => item.id);
+  return meids;
+};
+export default getListStudentIds;
